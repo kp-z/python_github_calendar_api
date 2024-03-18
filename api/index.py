58b9ba30c882af0b29596be7e18a12df1c2105e9
KP <41053750+kp-z@users.noreply.github.com>
2024-03-18 22:53:59 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -7,17 +7,22 @@ import json
 def list_split(items, n):
     return [items[i:i + n] for i in range(0, len(items), n)]
 def getdata(name):
-    gitpage = requests.get("https://github.com/kp-z")
+    gitpage = requests.get("https://github.com/" + name)
     data = gitpage.text
-    print( "All data: ", data)
-    datadatereg = re.compile(r'data-date="(.*?)" data-level')
-    datacountreg = re.compile(r'<span class="sr-only">(.*?) contribution')
+    
+    # 2023-11-22 更新正则 https://github.com/Zfour/python_github_calendar_api/issues/18
+    datadatereg = re.compile(r'data-date="(.*?)" id="contribution-day-component')
+    datacountreg = re.compile(r'<tool-tip .*?class="sr-only position-absolute">(.*?) contribution')
+    
     datadate = datadatereg.findall(data)
-    print( "datadate: ", datadate)
     datacount = datacountreg.findall(data)
-    print("Data account: " , datacount)    
     datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
 
+    # 检查datadate和datacount是否为空
+    if not datadate or not datacount:
+        # 处理空数据情况
+        return {"total": 0, "contributions": []}
+        
     # 将datadate和datacount按照字典序排序
     sorted_data = sorted(zip(datadate, datacount))
     datadate, datacount = zip(*sorted_data)
@@ -35,10 +40,13 @@ def getdata(name):
     return returndata
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        data = getdata('kp-z')
+        # 2024-02-21 固定用户名 https://github.com/Zfour/python_github_calendar_api/issues/20
+        # path = self.path
+        # user = path.split('?')[1]
+        user = 'kp-z'
+        data = getdata(user)
         self.send_response(200)
         self.send_header('Access-Control-Allow-Origin', '*')
         self.send_header('Content-type', 'application/json')
         self.end_headers()
         self.wfile.write(json.dumps(data).encode('utf-8'))
-        return
