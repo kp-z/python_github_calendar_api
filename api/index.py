3d0a9a8f0bde965b7d1d9fd3bae76a98c8b6b255
KP <41053750+kp-z@users.noreply.github.com>
2024-03-31 22:56:04 +0800

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,15 +1,14 @@
 # -*- coding: UTF-8 -*-
+pip install beautifulsoup4
 import requests
-import re
 from http.server import BaseHTTPRequestHandler
 import json
+from bs4 import BeautifulSoup
 
 def list_split(items, n):
     return [items[i:i + n] for i in range(0, len(items), n)]
-def getdata(name):
 
-    # 2024-03-29 定义 headers 请求头
-    # 请见 https://github.com/yuhengwei2001/python_github_calendar_api/commit/0f37cfc003f09e99a1892602d8bc2b38137899d2#diff-b014e93fcab9bae29f453d7a616da5eac2f02947f32d02a1a1bf200eeaab5a39L11
+def getdata(name):
     headers = {
         'Referer': 'https://github.com/'+ name,
         'Sec-Ch-Ua': '"Chromium";v="122", "Not(A:Brand";v="24", "Microsoft Edge";v="122"',
@@ -21,31 +20,22 @@ def getdata(name):
         'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/122.0.0.0 Safari/537.36 Edg/122.0.0.0',
         'X-Requested-With': 'XMLHttpRequest'
     }
-    # 发送请求时添加 headers 请求头
-    # gitpage = requests.get("https://github.com/" + name)
+
     gitpage = requests.get("https://github.com/" + name  + "?action=show&controller=profiles&tab=contributions&user_id="+ name, headers=headers)
     data = gitpage.text
-    
-    # 2023-11-22 更新正则 https://github.com/Zfour/python_github_calendar_api/issues/18
-    datadatereg = re.compile(r'data-date="(.*?)" id="contribution-day-component')
-    match = datadatereg.search(data)
-    if match:
-        print('Date:', match.group(1)) 
-    datacountreg = re.compile(r'<tool-tip .*?class="sr-only position-absolute">(.*?) contribution')
-    datadate = datadatereg.findall(data)
-    print(datadate)
-    datacount = datacountreg.findall(data)
-    datacount = list(map(int, [0 if i == "No" else i for i in datacount]))
 
-    # 检查datadate和datacount是否为空
+    soup = BeautifulSoup(data, 'html.parser')
+    contributions = soup.find_all('rect')
+
+    datadate = [contrib['data-date'] for contrib in contributions]
+    datacount = [int(contrib['data-count']) for contrib in contributions]
+
     if not datadate or not datacount:
-        # 处理空数据情况
         return {"total": 0, "contributions": []}
-        
-    # 将datadate和datacount按照字典序排序
+
     sorted_data = sorted(zip(datadate, datacount))
     datadate, datacount = zip(*sorted_data)
-    
+
     contributions = sum(datacount)
     datalist = []
     for index, item in enumerate(datadate):
@@ -57,9 +47,9 @@ def getdata(name):
         "contributions": datalistsplit
     }
     return returndata
+
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
-        # 2024-03-15 规范接口的传参方式 https://github.com/Zfour/python_github_calendar_api/issues/20#issuecomment-1999115747
         path = self.path
         spl=path.split('?')[1:]
         for kv in spl:
